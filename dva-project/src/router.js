de7a1ca18392c944090a8edefc4c2766d1b5b145
Jie ren Tian <110873204@qq.com>
2017-08-17 16:37:45 +0800

    dva..

---
+++ dva-project/src/router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router, Route } from 'dva/router';
+import IndexPage from './routes/IndexPage';
+
+import Users from "./routes/Users.js";
+
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Route path="/" component={IndexPage} />
+      <Route path="/users" component={Users} />
+      <Route path="/users" component={Users} />
+    </Router>
+  );
+}
+
+export default RouterConfig;
